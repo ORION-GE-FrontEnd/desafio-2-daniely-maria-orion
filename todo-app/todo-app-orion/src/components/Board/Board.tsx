bcbb4c175c7859869e2ea4484925dd4277672d34
Maria de Fátima <mnunesalves334@gmail.com>
2025-06-23 11:24:19 -0300

    fix: Refatoração de código para melhorar a legibilidade do código

--- todo-app/todo-app-orion/src/components/Board/Board.tsx
+++ todo-app/todo-app-orion/src/components/Board/Board.tsx
@@ -1,20 +1,21 @@
 import ListaDeTarefas from '../ListaDeTarefas/ListaDeTarefas';
 import Footer from "../Footer/Footer";
+import listas from '../../data/listas_tarefa.json'
 
 const Board = () => {
-    const listas = [
-        { titulo: "A fazer", exibirBotaoCriarTarefas: true, corDeFundo: "bg-pink-200" },
-        { titulo: "Em Andamento", exibirBotaoCriarTarefas: true, corDeFundo: "bg-yellow-100" },
-        { titulo: "Em Revisão", exibirBotaoCriarTarefas: true, corDeFundo: "bg-green-100" },
-        { titulo: "Concluído", exibirBotaoCriarTarefas: true, corDeFundo: "bg-green-200" }
-    ];
+    type Lista = {
+        titulo: string;
+        exibirBotaoCriarTarefas: boolean;
+        corDeFundo: string;
+    };
+
     return (
         <>
             <div className="bg-white min-h-screen">
                 <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-6">
                     <h1 className="text-black font-bold text-2xl sm:text-3xl md:text-4xl">Board</h1>
                     <main className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 mt-6">
-                        {listas.map((lista, index) => (
+                        {(listas as Lista[]).map((lista, index) => (
                             <ListaDeTarefas
                                 key={index}
                                 titulo={lista.titulo}
