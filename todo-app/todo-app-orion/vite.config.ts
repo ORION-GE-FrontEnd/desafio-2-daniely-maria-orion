4190b1593952e720295b5841bb1e08f7515a0376
Maria de Fátima <mnunesalves334@gmail.com>
2025-06-30 15:03:45 -0300

    chore: adicionar configuração de testes para rodar corretamente o Vitest

--- todo-app/todo-app-orion/vite.config.ts
+++ todo-app/todo-app-orion/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
-import tailwindcss from "@tailwindcss/vite"
 import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +8,9 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/setupTests.js',
+  },
 })
