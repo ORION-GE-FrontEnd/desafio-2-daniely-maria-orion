e0b7b9f49446da57892521d42df4cdec66bfa379
DanielyVasconcelos2024 <daniely.vasconcelos@academico.uncisal.edu.br>
2025-07-03 13:44:25 -0300

    feat(e2e): Adiciona testes e2e com Cypress mo Todo App

--- todo-app/todo-app-orion/src/components/Board/Board.tsx
+++ todo-app/todo-app-orion/src/components/Board/Board.tsx
@@ -13,8 +13,8 @@ const Board = () => {
         <>
             <div className="bg-white min-h-screen">
                 <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-6">
-                    <h1 className="text-black font-bold text-2xl sm:text-3xl md:text-4xl">Board</h1>
-                    <main className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 mt-6">
+                    <h1 className="text-black font-bold text-2xl sm:text-3xl md:text-4xl" data-cy="board-title">Board</h1>
+                    <main className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 mt-6" data-cy="board-main-content">
                         {(listas as Lista[]).map((lista, index) => (
                             <ListaDeTarefas
                                 key={index}
